017e31a388d8df1da051bd073d3b445412970bf3
Matthew Brabham <brabham.matthew@gmail.com>
2022-04-09 20:07:49 -0700

    Using Drupal date formatter and date time service to get current date and time formatted for the sites timezone.

--- src/Plugin/Block/StaticTimestamp.php
+++ src/Plugin/Block/StaticTimestamp.php
@@ -2,7 +2,11 @@
 
 namespace Drupal\generate_timestamp\Plugin\Block;
 
+use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'StaticTimestamp' block.
@@ -12,14 +16,54 @@ use Drupal\Core\Block\BlockBase;
  *  admin_label = @Translation("Static timestamp"),
  * )
  */
-class StaticTimestamp extends BlockBase {
+class StaticTimestamp extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The Date Time Formatter.
+   *
+   * @var Drupal\Core\Datetime\DateFormatterInterface
+   */
+  private $dateFormatter;
+
+  /**
+   * Drupal Time Service.
+   *
+   * @var Drupal\Component\Datetime\TimeInterface
+   */
+  private $timeService;
+
+  /**
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
+   *   The Date Time Formatter Interface.
+   * @param \Drupal\Component\Datetime\TimeInterface $time_service
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, DateFormatterInterface $date_formatter, TimeInterface $time_service) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->dateFormatter = $date_formatter;
+    $this->timeService = $time_service;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('date.formatter'), $container->get('datetime.time'));
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
+    $current_time = $this->timeService->getCurrentTime();
+    $date_today = $this->dateFormatter->format($current_time, 'custom', 'r');
     $build = [];
-    $build['static_timestamp']['#markup'] = $this->t('Timestamp that this block was generated @time', ['@time' => time()]);
+    $build['static_timestamp']['#markup'] = $this->t('Timestamp that this block was generated @time', ['@time' => $date_today]);
     return $build;
   }
 
--- src/UserTimestampGenerator.php
+++ src/UserTimestampGenerator.php
@@ -2,29 +2,49 @@
 
 namespace Drupal\generate_timestamp;
 
+use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Cache\Cache;
+use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Security\TrustedCallbackInterface;
 
 /**
  * Class UserTimestampGenerator.
  */
-class UserTimestampGenerator {
+class UserTimestampGenerator implements TrustedCallbackInterface {
+
+  /**
+   * @var \Drupal\Core\Datetime\DateFormatterInterface
+   */
+  private DateFormatterInterface $date_formatter;
+
+  /**
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  private TimeInterface $time;
 
   /**
    * Constructs a new UserTimestampGenerator object.
    */
-  public function __construct() {
-
+  public function __construct(DateFormatterInterface $date_formatter, TimeInterface $time) {
+    $this->date_formatter = $date_formatter;
+    $this->time = $time;
   }
 
   /**
    * Generate a timestamp.
    */
   public function generateUserTimestamp() {
+    $current_time = $this->time->getCurrentTime();
+    $date_today = $this->date_formatter->format($current_time, 'custom', 'r');
     // Invalidate the tag.
     Cache::invalidateTags(['timestamp']);
     return [
-      '#markup' => time(),
+      '#markup' => $date_today,
     ];
+  }
+
+  public static function trustedCallbacks() {
+    return ['generateUserTimestamp'];
   }
 
 }
